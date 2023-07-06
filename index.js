10cc295f37af89970471cf4398fc275d75a280f5
Vladislav <anitvlad288@gmail.com>
2023-07-06 07:28:29 +0300

    ?

---
+++ index.js
@@ -0,0 +1,19 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+let mongodb = require('mongodb');
+
+const MongoDBclient = new MongoClient('mongodb://Timeweb:cloud@127.0.0.1:27017/?authMechanism=DEFAULT');
+
+const connect = async () =>{
+    try {
+        await MongoDBclient.connect()
+        console.log("Успешно подключились к базе данных")
+        await MongoDBclient.close()
+        console.log("Закрыли подключение")
+    } catch (e) {
+        console.log(e)
+    }
+ }
+ 
+ connect()
